edd88054270295187dd31aa4b08ea345c4f4eb15
saasbook <saasbook@saasbook.(none)>
2015-11-28 07:24:32 -0400

    added password to the\user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+
+# Creat a virtual attribute "password"
+# A virtual attribute is an attribute not corresponding to a column in DB.
+attr_accessor :password
+attr_accessible :email, :name, :password, :password_confirmation
+
   attr_accessible :email, :name
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +14,31 @@ class User < ActiveRecord::Base
   validates :email,:presence =>true, 
                    :format => {:with => email_regex },
                    :uniqueness =>{ :case_sensitive =>false}
-end
+# Add validations to the "password" attribute
+# Also automatically create the virtual attribute     "password_confirmation".
+validates :password, :presence => true,
+                     :confirmation => true, # create "password_confirmation"
+                     :length => { :within => 6..40}
+before_save :encrypt_password
+
+  def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+  end
+  def self.authenticate(email, submitted_password)
+     user = find_by_email(email)
+     return nil if user.nil?
+     return user if user.has_password?(submitted_password)
+     return nil
+  end
+ private
+   def encrypt_password
+       self.encrypted_password = encrypt(password)
+   end
+   def encrypt(string)
+      Digest::SHA2.hexdigest(string)
+        # a temporay implementation, will be changed later.
+   end
+   end
+
+
+
